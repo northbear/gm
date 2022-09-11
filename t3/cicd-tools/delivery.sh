26bb376f8d82b4311bcdf5828f013449b000c0f8
Igor Yurchenko <harry.urcen@gmail.com>
2022-09-11 17:19:15 +0300

    add a code to cicd-tools scripts and add it to ubuntu-builder docker image

--- t3/cicd-tools/delivery.sh
+++ t3/cicd-tools/delivery.sh
@@ -1,1 +1,18 @@
 #!/usr/bin/env bash
+
+if [[ ! -v DELIVERY_DEST ]]; then
+    echo "*** Delivery skipped ***"
+    echo "A delivery destination (envvar DELIVERY_DEST) is not defined"
+    exit 0
+fi
+
+DELIVERY_SOURCE=${1}
+
+if [[ -f "${DELIVERY_SOURCE}" ]]; then
+    echo "The file ${DELIVERY_SOURCE} is pushed to repository ${DELIVERY_DEST}"
+elif [[ -d "${DELIVERY_SOURCE}" ]]; then
+    echo "A content of the directory ${DELIVERY_SOURCE} is pushed to repository ${DELIVERY_DEST}"
+else
+    echo "Error: incorrect parameter are provided. The directory or the file ${DELIVERY_SOURCE} doesn't exist"
+    exit 1
+fi
--- t3/cicd-tools/deployment.sh
+++ t3/cicd-tools/deployment.sh
@@ -1,1 +1,18 @@
 #!/usr/bin/env bash
+
+if [[ ! -v DEPLOY_ENV ]]; then
+    echo "*** Delivery skipped ***"
+    echo "A delivery destination (envvar DEPLOY_ENV) is not defined"
+    exit 0
+fi
+
+DEPLOY_SOURCE=${1}
+
+if [[ -f "${DEPLOY_SOURCE}" ]]; then
+    echo "The file ${DEPLOY_SOURCE} is deployed to the environment ${DEPLOY_ENV}"
+elif [[ -d "${DEPLOY_SOURCE}" ]]; then
+    echo "A content of the directory ${DEPLOY_SOURCE} is deployed to the environment ${DEPLOY_ENV}"
+else
+    echo "Error: incorrect parameter are provided. The directory or the file ${DEPLOY_SOURCE} doesn't exist"
+    exit 1
+fi
